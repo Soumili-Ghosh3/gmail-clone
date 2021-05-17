85201d0e481326e6fa54bfd868c50f096bb4ef66
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-18 01:23:35 +0530

    Changes made

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import Compose from '../buttons/Compose'
+import { sidebarButtonItems } from '../../data/SidebarButtonItems'
+import VideocamIcon from '@material-ui/icons/Videocam';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
 
 function Sidebar() {
     return (
@@ -11,12 +14,15 @@ function Sidebar() {
 
             <SideButtonsWrapper>
                 {
-                    
+                    sidebarButtonItems.map(items => (
+                        <SidebarButtonItem>{items.icons} {items.text}</SidebarButtonItem>
+                    ))
                 }
             </SideButtonsWrapper>
 
             <MeetWrapper>
-
+                <Title> Meet</Title>
+                <p></p>
             </MeetWrapper>
 
             <HangoutsWrapper>
@@ -32,11 +38,32 @@ function Sidebar() {
 
 export default Sidebar
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+    border-right: 1px solid lightgray;
+    height: 100vh;
+`
 
-const ComposeWrapper = styled.div``
+const ComposeWrapper = styled.div`
+    display: grid;
+    place-items: start;
+    padding: 10px 20px;
+`
 
 const SideButtonsWrapper = styled.div``
+
+const SidebarButtonItem = styled.div`
+    display: grid;
+    grid-template-columns: 14% auto;
+    color: gray;
+    padding: 5px 25px;
+    border-radius: 0 100px 100px 0;
+    curosr: pointer;
+    margin-right: 8px;
+
+    :hover{
+        background-color: #f5f7f7;
+    }
+`
 
 const MeetWrapper = styled.div``
 
