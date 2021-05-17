2f86c9090c4fcd2b9fd027849114f3471c013ae5
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-17 23:19:59 +0530

    Added Sidebar

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Main() {
     return (
-        <div>
-            
-        </div>
+        <Wrapper>
+            <Sidebar>
+
+            </Sidebar>
+
+            <EmailsView>
+
+            </EmailsView>
+
+            <SideIcons>
+                
+            </SideIcons>
+        </Wrapper>
     )
 }
 
 export default Main
+
+const Wrapper = styled.div``
