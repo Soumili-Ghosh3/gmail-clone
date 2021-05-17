36a914a7927c3b04ffa617a5a60e785dd86a89e9
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-18 00:43:58 +0530

    Changes made

---
+++ src/components/data/SidebarButtonItem.js
@@ -0,0 +1,44 @@
+import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import SendIcon from '@material-ui/icons/Send';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import LabelIcon from '@material-ui/icons/Label';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+export const sidebarButtonItems = [
+    {
+        icons: <InboxIcon />,
+        text: 'Inbox'
+    },
+
+    {
+        icons: <StarIcon />,
+        text: 'Starred'
+    },
+
+    {
+        icons: <WatchLaterIcon />,
+        text: 'Snoozed'
+    },
+
+    {
+        icons: <SendIcon />,
+        text: 'Sent'
+    },
+
+    {
+        icons: <InsertDriveFileIcon />,
+        text: 'Drafts'
+    },
+
+    {
+        icons: <LabelIcon />,
+        text: 'Notes'
+    },
+
+    {
+        icons: <ExpandMoreIcon />,
+        text: 'More'
+    },
+]
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Compose from '../buttons/Compose'
 
 function Sidebar() {
     return (
@@ -9,7 +10,9 @@ function Sidebar() {
             </ComposeWrapper>
 
             <SideButtonsWrapper>
-
+                {
+                    
+                }
             </SideButtonsWrapper>
 
             <MeetWrapper>
