216f73a95a94f157be155dea1e478a4bcafdd93d
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-18 12:53:52 +0530

    Added emails

--- src/components/layout/EmailsView.js
+++ src/components/layout/EmailsView.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
 
 function EmailsView() {
     return (
-        <div>
-            
-        </div>
+        <Wrapper>
+            <TopWrapper>
+                <IconButton>
+                   <CheckBoxOutlineBlankIcon />
+                </IconButton>
+    
+                <IconButton>
+                    <RefreshIcon />
+                </IconButton>
+                
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </TopWrapper>
+
+            <EmailsContainer>
+
+            </EmailsContainer>
+        </Wrapper>
     )
 }
 
 export default EmailsView
+
+const Wrapper = styled.div``
+
+const TopWrapper = styled.div`
+    padding-left: 20px;
+    height: 48px;
+    place-items: center;
+    align-items: center;
+`
+
+const EmailsContainer = styled.div``
---
+++ src/temp/EmailData.js
@@ -0,0 +1,25 @@
+export const emailData  = [
+    {
+        starred: true,
+        from: 'Google',
+        message: 'Congratulations!!!!!',
+        received: '10:30',
+        read: 'true'
+    }
+
+    {
+        starred: false,
+        from: 'Jasmine',
+        message: 'Yooo Broo',
+        received: '21:12',
+        read: 'false'
+    }
+
+    {
+        starred: false,
+        from: 'David',
+        message: 'What are u doing?',
+        received: '17:50',
+        read: 'true'
+    }
+]
